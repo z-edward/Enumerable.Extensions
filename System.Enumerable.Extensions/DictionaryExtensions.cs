a091cc65e24e35aab699985d3391c69a3308e96d
Журавлев Эдуард <zedward@outlook.com>
2021-04-06 23:38:23 +0300

    Remove IDictionary.GetOrAdd extension method

--- System.Enumerable.Extensions/DictionaryExtensions.cs
+++ System.Enumerable.Extensions/DictionaryExtensions.cs
@@ -19,18 +19,6 @@ namespace System.Enumerable.Extensions {
             return @this.ContainsKey(key) ? @this[key] : @default;
         }
 
-       /// <summary>
-        /// Get item from dictionary, or default value
-        /// </summary>
-        /// <typeparam name="TKey">Type of key</typeparam>
-        /// <typeparam name="T">Type of value</typeparam>
-        /// <param name="this">Dictionary</param>
-        /// <param name="key">Key value</param>
-        /// <param name="default">Default value</param>
-        /// <returns>Value or default value</returns>
-        public static T GetOrDefault<TKey, T>(this IDictionary<TKey, T> @this, TKey key, T @default = default) {
-            return @this.ContainsKey(key) ? @this[key] : @default;
-        }
 
         /// <summary>
         /// Get value from dictionary, or add it, if not exists
@@ -44,7 +32,7 @@ namespace System.Enumerable.Extensions {
         public static T GetOrdAdd<TKey, T>(this IDictionary<TKey, T> @this, TKey key, Func<T> add) {
 
             T factory() { T v = add(); @this.Add(key, v ); return v; }
-            return @this.GetOrDefault(key, factory());
+            return @this.ContainsKey(key) ? @this[key] : factory();
         } 
         
     }
